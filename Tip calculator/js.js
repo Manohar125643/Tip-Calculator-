655b034d647f4e0758f114698bff23fb58b58a27
Manohar125643 <43780171+Manohar125643@users.noreply.github.com>
2020-11-04 00:01:47 +0530

    Add files via upload

--- Tip calculator/js.js
+++ Tip calculator/js.js
@@ -1,41 +1,50 @@
 function Tipcalculator() {
- 			var amount = document.getElementById("amount").value;
-  		 	var feedback = document.getElementById("feedback").value;
- 		 	var numOfPeople = document.getElementById("spiltinto").value;
+    var amount = document.getElementById("amount").value;
+    var feedback = document.getElementById("feedback").value;
+    var numOfPeople = document.getElementById("spiltinto").value;
 
 
-			  if (amount === "" || feedback == 0) {
-			    alert("Please enter values");
-			    return;
-			  }
-			   if(numOfPeople<=0)
-			  {
-			    alert("Please enter correct value");
-			    return;
-			  }
+    var amountElement = document.getElementById("amount");
+    if (!amountElement.checkValidity()) {
+        document.getElementById("amountValidation").innerHTML = amountElement.validationMessage;
+        return;
+    }
 
-			  var total = (amount * feedback) / numOfPeople;
-			  total = Math.round(total);
-			  total = total.toFixed(2);
+    if (feedback == 0) {
+        alert("Slect an option");
+        return;
+    }
 
-			  var totalt = (amount * feedback);
-			  totalt = Math.round(totalt);
-			  totalt = totalt.toFixed(2);
-
-			  document.getElementById("totalTip").style.display = "block";
-			  document.getElementById("tip").innerHTML = total;
-
-			  document.getElementById("t_tipp").style.display = "block";
-			  document.getElementById("t_tip").innerHTML = totalt;
-
-			}
+    var numOfPeopleElement = document.getElementById("spiltinto");
+    if (!numOfPeopleElement.checkValidity()) {
+        document.getElementById("numberofpeopleValidation").innerHTML = numOfPeopleElement.validationMessage;
+        return;
+    }
 
 
-				document.getElementById("totalTip").style.display = "none";
-				document.getElementById("t_tipp").style.display = "none";
-				
 
-				document.getElementById("calculate").onclick = function() {
-				  Tipcalculator();
+    var total = (amount * feedback) / numOfPeople;
+    //total = Math.round(total);
+    total = total.toFixed(2);
 
-				};
+    var totalt = (amount * feedback);
+    //totalt = Math.round(totalt);
+    totalt = totalt.toFixed(2);
+
+    document.getElementById("totalTip").style.display = "block";
+    document.getElementById("tip").innerHTML = total;
+
+    document.getElementById("t_tipp").style.display = "block";
+    document.getElementById("t_tip").innerHTML = totalt;
+
+}
+
+
+document.getElementById("totalTip").style.display = "none";
+document.getElementById("t_tipp").style.display = "none";
+
+
+document.getElementById("calculate").onclick = function() {
+    Tipcalculator();
+
+};
